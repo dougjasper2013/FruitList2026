8ae596d2e2d4d0adaacef78ff76dfb7581cd868f
Douglas Jasper <doug.jasper@trios.com>
2026-07-15 09:30:42 -0400

    Just prior to break

--- FruitList2026/ContentView.swift
+++ FruitList2026/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var fruits: [Fruit] = []
+    @State private var searchText = ""
+    @State private var isLoading = true
+    @State private var errorMessage: String?
+    
+    // Filtered list based on search text
+    var filteredFruits: [Fruit] {
+        if searchText.isEmpty {
+            return fruits
+        } else {
+            return fruits.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
         }
-        .padding()
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Group {
+                if isLoading {
+                    ProgressView("Loading Fruits...")
+                } else if let errorMessage = errorMessage {
+                    Text("Error: \(errorMessage)")
+                        .foregroundColor(.red)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                } else {
+                    List(filteredFruits) { fruit in
+                        NavigationLink(value: fruit) {
+                            Text(fruit.name)
+                                .font(.headline)
+                        }
+                    }
+                    .navigationDestination(for: Fruit.self) { fruit in
+                        FruitDetailView(fruit: fruit)
+                    }
+                    .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search fruits")
+                }
+            }
+            .navigationTitle("Fruits")
+            .task {
+                await loadFruits()
+            }
+        }
+    }
+    
+    func loadFruits() async {
+        do {
+            fruits = try await FruitService.shared.fetchFruits()
+            isLoading = false
+        } catch {
+            errorMessage = error.localizedDescription
+            isLoading = false
+        }
     }
 }
 
--- FruitList2026/FruitDetailView.swift
+++ FruitList2026/FruitDetailView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct FruitDetailView: View {
+    
+    let fruit: Fruit
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    FruitDetailView()
+    FruitDetailView(fruit: Fruit(
+        name: "Apple",
+        family: "Rosaceae",
+        order: "Rosales",
+        nutritions: Nutrition(carbohydrates: 22, protein: 5, fat: 3 , calories: 100, sugar: 17.2)
+    ))
 }
