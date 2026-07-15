c3ba1f33edddac04e9b1e7ba04b28b3ebefbf686
Douglas Jasper <doug.jasper@trios.com>
2026-07-15 08:46:35 -0400

    Initial Commit

---
+++ FruitList2026/FruitList2026App.swift
@@ -0,0 +1,17 @@
+//
+//  FruitList2026App.swift
+//  FruitList2026
+//
+//  Created by Douglas Jasper on 2026-07-15.
+//
+
+import SwiftUI
+
+@main
+struct FruitList2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FruitList2026Tests/FruitList2026Tests.swift
@@ -0,0 +1,19 @@
+//
+//  FruitList2026Tests.swift
+//  FruitList2026Tests
+//
+//  Created by Douglas Jasper on 2026-07-15.
+//
+
+import Testing
+@testable import FruitList2026
+
+struct FruitList2026Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
